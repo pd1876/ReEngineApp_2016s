932c28c2db40a2c0b53fc668689e2cd04b1f7c9c
Pedro DelaCuadra <pd1876@g.rit.edu>
2017-02-26 11:38:24 -0500

    A05-Transformartions. Earth and Moon translations working, can't figure out rotations.

--- A05_Transformations/AppClass.cpp
+++ A05_Transformations/AppClass.cpp
@@ -50,13 +50,14 @@ void AppClass::Update(void)
 
 #pragma region YOUR CODE GOES HERE
 	//Calculate the position of the Earth
-	m_m4Earth = glm::rotate(rotateX, m_fEarthTimer, vector3(0.0f, 1.0f, 0.0f));
-	m_m4Earth = glm::translate(m_m4Sun, vector3(sin(m_fEarthTimer) * 11.0f, 0.0f, cos(m_fEarthTimer) * 11.0f));
-	// 
-	//  
+	m_m4Earth = glm::translate(m_m4Sun, vector3(cos(m_fEarthTimer/28) * 11.0f, 0.0f, sin(m_fEarthTimer/28) * 11.0f));
+	m_m4Earth = glm::rotate(m_m4Earth, m_fEarthTimer, vector3(0.0f, 1.0f, 0.0f));
+	m_m4Earth = glm::translate(m_m4Sun, vector3(-(cos(m_fEarthTimer/28) * 11.0f), 0.0f, -(sin(m_fEarthTimer/28) * 11.0f)));
+
 	//Calculate the position of the Moon
+	m_m4Moon = glm::translate(m_m4Earth, vector3(cos(m_fMoonTimer/28)*2.0f, 0.0f, sin(m_fMoonTimer/28)*2.0f));
 	m_m4Moon = glm::rotate(m_m4Moon, m_fMoonTimer, vector3(0.0f, 1.0f, 0.0f));
-	m_m4Moon = glm::translate(m_m4Earth, vector3(sin(m_fMoonTimer) * 2.0f, 0.0f, cos(m_fMoonTimer) * 2.0f));
+	m_m4Moon = glm::translate(m_m4Earth, vector3(-(cos(m_fMoonTimer/28)*2.0f), 0.0f, -(sin(m_fMoonTimer/28)*2.0f)));
 #pragma endregion
 
 #pragma region Print info
